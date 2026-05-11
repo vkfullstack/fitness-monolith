bf1add61c47c8562bffc6807048abe4f5ca06f50
vkfullstack <vijaykhetre36@gmail.com>
2026-05-11 22:55:35 +0530

    Created ActivityType enum for fitness activity categories

---
+++ src/main/java/com/project/fitness/model/ActivityType.java
@@ -0,0 +1,15 @@
+package com.project.fitness.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+
+}
